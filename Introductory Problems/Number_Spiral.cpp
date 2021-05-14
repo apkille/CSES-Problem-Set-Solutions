1ea3a9949da197f961a4a3218b3bbae9487cb95a
killeandrew <killeandrew@gmail.com>
2021-05-14 08:13:18 -0400

    not finished with this problem

---
+++ Introductory Problems/Number_Spiral.cpp
@@ -0,0 +1,26 @@
+/*
+    author: kille
+*/
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+ 
+using namespace std;
+ 
+ void solve()
+ {
+     int y, x;
+     cin >> y >> x;
+
+     
+ }
+ int main()
+ {
+     int t;
+     cin >> t;
+     while (t--)
+     {
+         solve();
+     }
+ }
